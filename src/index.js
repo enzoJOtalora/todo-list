2221ea43ee631bf6b98ea9fc087607409323d1bf
enzoJOtalora <126079379+enzoJOtalora@users.noreply.github.com>
2023-10-26 09:07:48 -0300

    Create todoMaker factory function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,21 @@
-console.log("Hello");
+const priorityLevels = ['LOW','MEDIUM','HIGH'];
+
+const todoMaker = () => {
+    return {
+        description: '',
+        day: '',
+        month: '',
+        year: '',
+        complete: false,
+        priority: 'LOW',
+        printDate(){
+            return this.day+'/'+this.month+'/'+this.year;
+        },
+        toggleComplete(){
+            this.complete=!this.complete;
+        },
+        changePriority(index){
+            this.priority=priorityLevels[index];
+        }
+    }
+}

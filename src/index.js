80bf23768566431fb6f9f00f42858712677dccf5
enzoJOtalora <126079379+enzoJOtalora@users.noreply.github.com>
2023-10-26 11:27:49 -0300

    Add selectors and form inputs in JS

--- src/index.js
+++ src/index.js
@@ -1,14 +1,36 @@
 import './style.css';
 
 const element = document.createElement("div");
-element.innerHTML="<h1>Hello!</h1>";
+element.innerHTML="<hr/><h1>SCRIPT LOADER</h1>";
 document.body.appendChild(element);
 
 const priorityLevels = ['LOW','MEDIUM','HIGH'];
 
+//ToDo dialog group
+const todoDialog = document.querySelector("#todo-modal");
+const todoName = document.getElementById("#name");
+const todoDetails = document.getElementById("#details");
+const todoDate = document.getElementById("#date");
+const todoPrio = document.getElementById("#prio");
+const todoAddBtn = document.querySelector("#new-todo");
+const todoCloseBtn = document.querySelector("#close-dialog");
+const todoForm = document.querySelector("#todo-form");
+
+todoAddBtn.addEventListener('click', ()=>{
+    todoDialog.showModal();
+})
+todoCloseBtn.addEventListener('click',()=>{
+    todoDialog.close();
+})
+
+todoForm.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    
+})
+
 const todoMaker = () => {
     return {
-        title: '',
+        name: '',
         description: '',
         day: '',
         month: '',
@@ -33,4 +55,15 @@ const projectMaker = () => {
         description: '',
         toDoArray: [],
     }
+}
+
+function logToDo(){
+    let newTodo = todoMaker();
+    newTodo.name = todoName.value;
+    newTodo.details = todoDetails.value;
+    let date = todoDate.value.split('-');
+    newTodo.day = date[2];
+    newTodo.month = date[1];
+    newTodo.year = date[0];
+    newTodo.priority = todoPrio.value;
 }

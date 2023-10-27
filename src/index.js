9cb37accc30efeb6e62b8c327f2df2b60b1a6449
enzoJOtalora <126079379+enzoJOtalora@users.noreply.github.com>
2023-10-27 09:49:49 -0300

    Write project logger and form selectors

--- src/index.js
+++ src/index.js
@@ -6,15 +6,28 @@ document.body.appendChild(element);
 
 const priorityLevels = ['LOW','MEDIUM','HIGH'];
 
+const todoAddBtn = document.querySelector("#new-todo");
+const projectAddBtn = document.querySelector("#new-project");
+
 //ToDo dialog group
 const todoDialog = document.querySelector("#todo-modal");
 const todoName = document.getElementById("#name");
-const todoDetails = document.getElementById("#details");
-const todoDate = document.getElementById("#date");
-const todoPrio = document.getElementById("#prio");
-const todoAddBtn = document.querySelector("#new-todo");
+const todoDetails = document.getElementById("details");
+const todoDate = document.getElementById("date");
+const todoPrio = document.getElementById("prio");
 const todoCloseBtn = document.querySelector("#close-dialog");
 const todoForm = document.querySelector("#todo-form");
+
+//Project dialog group
+
+const projectDialog = document.querySelector("#project-modal");
+const projectName = document.getElementById("projectName");
+const projectDetails = document.getElementById("projectDescription");
+const projectCloseBtn = document.querySelector("#close-dialog-project");
+const projectForm = document.querySelector("#project-form");
+
+let myTodos = [];
+let myProjects = [];
 
 todoAddBtn.addEventListener('click', ()=>{
     todoDialog.showModal();
@@ -23,9 +36,17 @@ todoCloseBtn.addEventListener('click',()=>{
     todoDialog.close();
 })
 
+projectAddBtn.addEventListener('click', ()=>{
+    projectDialog.showModal();
+})
+projectCloseBtn.addEventListener('click',()=>{
+    projectDialog.close();
+})
+
 todoForm.addEventListener('submit', (e)=>{
     e.preventDefault();
-    
+    logToDo();
+
 })
 
 const todoMaker = () => {
@@ -66,4 +87,29 @@ function logToDo(){
     newTodo.month = date[1];
     newTodo.year = date[0];
     newTodo.priority = todoPrio.value;
+    return newTodo;
+}
+
+function logProject(){
+    let newProject = projectMaker();
+    newProject.title = projectName.value;
+    newProject.details = projectDetails.value;
+    return newProject;
+}
+
+function checkStorage(){
+    
+    if (localStorage.getItem("myTodos")){
+        myTodos = JSON.parse(localStorage.getItem("myTodos"));
+        console.log(JSON.parse(localStorage.getItem("myTodos")));
+    }
+    if (localStorage.getItem("myProjects")) {
+        myProjects = JSON.parse(localStorage.getItem("myProjects"));
+        console.log(JSON.parse(localStorage.getItem("myProjects")));
+      } 
+}
+
+function updateStorage(){
+    localStorage.setItem("myLibrary", JSON.stringify(myLibrary));
+    localStorage.setItem("myTodos", JSON.stringify(myTodos));
 }
